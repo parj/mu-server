e24f7dbeff93ef67986aa271978518f84bb941a9
Daniel Flower <git@danielflower.com>
2019-06-16 19:20:30 +0800

    Timeout investigation

--- src/test/java/io/muserver/MuServerTest.java
+++ src/test/java/io/muserver/MuServerTest.java
@@ -404,12 +404,12 @@ public class MuServerTest {
 
             });
         }
-        log.info("4");
+//        log.info("4");
 
         MuAssert.assertNotTimedOut("firstRequestStartedLatch", firstRequestStartedLatch);
-//        log.info("5");
+        log.info("5");
         try (Response resp = call(request(server.uri().resolve("/?count=last")))) {
-            log.info("7");
+//            log.info("7");
             assertThat(resp.code(), is(503));
             assertThat(resp.body().string(), is("503 Service Unavailable"));
         }
